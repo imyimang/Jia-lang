dc2dceee5d01300330acb4409520c251ba759e0d
yimang <lucasbbtung@gmail.com>
2024-07-10 02:10:53 +0800

    edit compiler

--- README.md
+++ README.md
@@ -1,2 +1,28 @@
-a new programming language made with python
+# 甲語言 Jia
+一個基於brainfuck改編而成的程式語言
+
+## 程式語法
+`由` 輸出，對應brainfuck中的`.`
+
+`甲` 輸入，對應brainfuck中的`,`
+
+`左` 將指標往左移一格，對應brainfuck中的`<`
+
+`右` 將指標往右移一格，對應brainfuck中的`>`
+
+`申` 對當前的位元組+1，對應brainfuck中的`+`
+
+`申` 對當前的位元組-1，對應brainfuck中的`-`
+
+`始` 若指標所指位元組的值為零，則向後跳轉，跳轉到其對應的`終`的下一個指令處，對應brainfuck中的`[`
+
+`終` 若指標所指位元組的值不為零，則向前跳轉，跳轉到其對應的`始`的下一個指令處，對應brainfuck中的`]`
+
+## 編輯器操作
+`jia` 直接在 REPL(Read-Eval-Print Loop) 中運行Jia，輸入 `exit` 離開
+
+`run (file name)` 執行指定的檔案，僅支援 `.txt` 和 `.jia`，如果沒有設定副檔名，預設情況下會先找尋 `(file name).jia` 再找尋 `(file name).txt`，如果兩者皆不存在則報錯
+
+`edit (file name)` 在指定的檔案追加文字，僅支援 `.txt` 和 `.jia`，如果沒有設定副檔名，預設情況下會先找尋 `(file name).jia` 再找尋 `(file name).txt`，如果兩者皆不存在則創建 `(file name).jia`，**該功能只能追加文字，無法編輯先前寫的內容**，按下 `Ctrl + C` 來結束編輯
+
 # Not done yet
--- compiler.py
+++ compiler.py
@@ -1,6 +1,24 @@
-from setup import run_jia 
+from repl import run_jia 
 import os,re,sys
 import subprocess
+
+
+def edit_file(file_name):
+    with open(file_name, "a+", encoding='utf-8') as f:
+        print(f"Editing {file_name}, Press Ctrl+C to exit...\n")
+        f.seek(0)
+        print(f.read(),end="")
+        f.seek(0, 2)
+        input_lines = []
+        try:
+            while True:
+                line = sys.stdin.readline().rstrip('\n')
+                input_lines.append(line)
+                f.write("\n" + line)
+        except KeyboardInterrupt:
+            print(f"\n\n...End editing {file_name}")
+
+
 print("Welcome to Jia! Enter 'exit' to exit...")
 while True:
     user_input = input("Jia> ")
@@ -14,37 +32,44 @@ while True:
 
     elif list(user_input.split(" "))[0].lower() == "run" and len(list(user_input.split(" "))) == 2:
         inputs = list(user_input.split(" "))
-        if os.path.exists(inputs[1]) and os.path.isfile(inputs[1]):
+        if (os.path.exists(inputs[1]) and os.path.isfile(inputs[1])):
             root, extension = os.path.splitext(inputs[1])
             if extension == ".jia" or extension == ".txt":
                 with open(inputs[1], "r", encoding='utf-8') as f:
                     content = f.read()
                 run_jia(content)
             else:
-                print("Error: Unknown file type(Only supports .txt and .jia)")    
+                print("Error: Unknown file type(Only supports .txt and .jia)")
+
+        elif re.match(r"^[^.]*$", inputs[1]):
+            if (os.path.exists(f"{inputs[1]}.jia") and os.path.isfile(f"{inputs[1]}.jia")):
+                with open(f"{inputs[1]}.jia", "r", encoding='utf-8') as f:
+                    content = f.read()
+                run_jia(content)
+            elif (os.path.exists(f"{inputs[1]}.txt") and os.path.isfile(f"{inputs[1]}.txt")):
+                with open(f"{inputs[1]}.txt", "r", encoding='utf-8') as f:
+                    content = f.read()
+                run_jia(content)
         else:
             print("Error: Invalid file name")
+
 
     elif list(user_input.split(" "))[0].lower() == "edit" and len(list(user_input.split(" "))) == 2:
         inputs = list(user_input.split(" "))
         if re.match(r"[^\.]+\.(jia|txt)", inputs[1]):
             root, extension = os.path.splitext(inputs[1])
+            edit_file(inputs[1])
 
         elif re.match(r"^[^.]*$", inputs[1]):
-            with open(f"{inputs[1]}.jia", "a+", encoding='utf-8') as f:
-                print(f"Editing {inputs[1]}.jia, Press Ctrl+C to exit...\n")
-                f.seek(0)
-                print(f.read(),end="")
-                f.seek(0, 2)
-                input_lines = []
-                try:
-                    while True:
-                        line = sys.stdin.readline().rstrip('\n')
-                        input_lines.append(line)
-                        f.write("\n" + line)
-                except KeyboardInterrupt:
-                    print()
-                #print("\n".join([i for i in input_lines]))
+            edit_file(f"{inputs[1]}.jia")
+        
+        else:
+            print("Error: Invalid file name")
+
+    else:
+        print("Error: Invalid command")
+
+
     
 
 
